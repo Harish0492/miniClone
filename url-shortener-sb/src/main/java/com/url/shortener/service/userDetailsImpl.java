e8ff091f54d6ae58ca137c77501eb10a8d939447
harish korukonda <korukondaharish321@gmail.com>
2025-03-14 20:20:24 -0500

    setting the tools

---
+++ url-shortener-sb/src/main/java/com/url/shortener/service/userDetailsImpl.java
@@ -0,0 +1,12 @@
+package com.url.shortener.service;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@NoArgsConstructor
+public class userDetailsImpl implements UserDetails {
+
+
+}
